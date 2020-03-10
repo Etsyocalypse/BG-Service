8e2cfa1efd5d8a84e2b081c414e424d592c362c4
Brian Gardner <divinebovine@gmail.com>
2020-03-10 17:05:26 -0500

    initial commit for deployment

---
+++ app/ItemInfoContainer/ItemInfoContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class ItemInfoContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>Item Info Container</h1>
+        );
+    }
+
+}
+
+export default ItemInfoContainer;
---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import ItemInfoContainer from './ItemInfoContainer'
+
+render(<ItemInfoContainer/>, document.getElementById('iteminfocontainer'))
