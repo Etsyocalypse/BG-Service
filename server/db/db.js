d1e0843bef8e8df38750f7c1cd5f953b37ea8c5a
Brian Gardner <59371085+bpg031000@users.noreply.github.com>
2020-03-23 19:50:57 -0500

    set up connection and basic query to mongoDB atlas cluster

--- server/db/db.js
+++ server/db/db.js
@@ -1,25 +1,39 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const mongoUser = require('../config').mongoUser;
+const mongoPass = require('../config').mongoPass;
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
 
-// Database Name
-const dbName = 'test';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  db.collection('testItems').find({_id: 111 }).toArray((err, results)=>{
+// const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cluster0-jdvtt.mongodb.net/test?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true }, {useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("etsyPoc").collection("items");
+  // perform actions on the collection object
+  collection.find({_id: 111 }).toArray((err, results)=>{
     console.log(err ? `err:${err}` : results);
   });
-
-  // client.close();
+  client.close();
 });
+
+
+// Database Name
+// const dbName = 'test';
+
+// // Create a new MongoClient
+// const client = new MongoClient(url);
+
+// // Use connect method to connect to the Server
+// client.connect(function(err) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+
+//   const db = client.db(dbName);
+
+//   db.collection('testItems').find({_id: 111 }).toArray((err, results)=>{
+//     console.log(err ? `err:${err}` : results);
+//   });
+
+//   // client.close();
+// })
